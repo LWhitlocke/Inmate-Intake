fbfa385ad1b9199742d27ebae9acb9407a741025
lwhitlocke <lwhitlocke@unilink.com>
2019-11-09 23:02:27 +0000

    inital checkin of project. Got base UI ready

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { LegendItem, ChartType } from '../lbd/lbd-chart/lbd-chart.component';
+import * as Chartist from 'chartist';
+import { TableData } from 'app/tables/table-data';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public tableData1: TableData;
+
+    public hoursChartType: ChartType;
+    public hoursChartData: any;
+    public hoursChartOptions: any;
+    public hoursChartResponsive: any[];
+    public hoursChartLegendItems: LegendItem[];
+
+  constructor() { }
+
+  ngOnInit() {
+      this.hoursChartType = ChartType.Line;
+      this.hoursChartData = {
+        labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
+        series: [
+          [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
+          [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
+          [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
+        ]
+      };
+      this.hoursChartOptions = {
+        low: 0,
+        high: 800,
+        showArea: true,
+        height: '245px',
+        axisX: {
+          showGrid: false,
+        },
+        lineSmooth: Chartist.Interpolation.simple({
+          divisor: 3
+        }),
+        showLine: false,
+        showPoint: false,
+      };
+      this.hoursChartResponsive = [
+        ['screen and (max-width: 640px)', {
+          axisX: {
+            labelInterpolationFnc: function (value) {
+              return value[0];
+            }
+          }
+        }]
+      ];
+      this.hoursChartLegendItems = [
+        { title: 'Open', imageClass: 'fa fa-circle text-info' },
+        { title: 'Click', imageClass: 'fa fa-circle text-danger' },
+        { title: 'Click Second Time', imageClass: 'fa fa-circle text-warning' }
+      ];
+
+      this.tableData1 = {
+        headerRow: [ 'ID', 'Name', 'Country', 'City', 'Salary'],
+        dataRows: [
+            ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
+            ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
+            ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
+            ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
+            ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
+            ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
+        ]
+    };
+    }
+
+}
